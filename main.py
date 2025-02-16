66e5cac001fca39e7640f06ca64ea988b486ca25
asritha26k <132151227+asritha26k@users.noreply.github.com>
2025-02-16 17:46:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
+import os
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
-from model_loader import predict  # Import directly from model_loader.py
+from model_loader import predict
 
 app = FastAPI()
 
 # Add CORS middleware to allow requests from your Chrome extension
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allow all domains for now, or specify your extension's URL
+    allow_origins=["*"],  # You can specify a list of allowed origins here for security
     allow_credentials=True,
     allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
     allow_headers=["*"],  # Allow all headers
@@ -26,3 +27,7 @@ class InputText(BaseModel):
 def get_prediction(input_data: InputText):
     result = predict(input_data.text)
     return {"modified_text": result}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
